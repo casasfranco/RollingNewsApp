c459cea8d4bbd659c841182b6134f1fef7a3a385
Lucas Gutierrez <lucasismaelgutierrez@gmail.com>
2021-01-04 18:27:07 -0300

    Se validaron los campos de nombre y descripcion de la seccion cateogoria

--- src/components/common/Administrador/Categorias/FormCat.js
+++ src/components/common/Administrador/Categorias/FormCat.js
@@ -2,20 +2,82 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Container } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const FormCat = () => {
+  const { register, errors, handleSubmit } = useForm();
+
+  const onSubmit = async (data, e) => {
+    console.log("anda perro");
+    //Estuctura del objeto de la base de datos
+    const categoria = { 
+      nombreCat: data.nombreCategoria,
+      descripcionCat: data.descripcionCategoria,
+      estadoCat: true,
+    };
+  }
+
+
   return (
     <div>
         <Container>
-      <Form className="my-5 bg-light p-3">
+      <Form className="my-5 bg-light p-3" onSubmit={handleSubmit(onSubmit)}>
         <Form.Group controlId="formBasicEmail">
           <Form.Label><h4>Nombre de la categoria:</h4></Form.Label>
-          <Form.Control type="text" placeholder="Actualidad" />
+          <Form.Control 
+          type="text" placeholder="Actualidad"
+          name="nombreCategoria"
+          ref={register({
+            required: {
+              value:true,
+              message: "Ingrese una categoria",
+            },
+            maxLength: {
+              value: 15,
+              message: "No más de 15 caracteres",
+            },
+            minLength: {
+              value: 5,
+              message: "Mínimo al menos 5 caracteres",
+            },
+            pattern: {
+              value: /^[A-Za-z\s]+$/,
+              message: "Ingrese una categoria válida",
+            }
+          })}
+          />
+          <span id="errorNombre" className="text-danger mb-2">
+            {errors?. nombreCategoria?.message}
+          </span>
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label><h4>Descripción:</h4></Form.Label>
-          <Form.Control type="text" placeholder="Noticias sobre..." />
+          <Form.Control type="text" 
+          placeholder="Noticias sobre..." 
+          name="descripcionCategoria"
+          ref={register({
+            required: {
+              value:true,
+              message: "Ingrese una descripción",
+            },
+            maxLength: {
+              value: 40,
+              message: "No más de 40 caracteres",
+            },
+            minLength: {
+              value: 10,
+              message: "Mínimo al menos 10 caracteres",
+            },
+            pattern: {
+              value: /^[A-Za-z\s]+$/,
+              message: "Complete correctamente el campo",
+            }
+          })}
+          />
+           <span id="errorDescripcion" className="text-danger mb-2">
+            {errors?.descripcionCategoria?.message}
+          </span>
         </Form.Group>
         <Button variant="danger" type="submit">
           Agregar
